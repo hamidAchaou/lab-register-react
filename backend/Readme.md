f6a7c88c3de0349328a9cb753e6e8cd469eedcb7
Hamid Achaou <achaou.hamid.solicode@gmail.com>
2024-08-12 01:00:14 +0100

    fix

---
+++ backend/Readme.md
@@ -0,0 +1,16 @@
+# lab FastApi python 
+
+1. install venv
+```bash
+cd backend
+python -m venv venv
+```
+2. install library :
+```bash
+   pip install -r requirements.txt
+```
+
+3. activate script 
+```bash
+venv\Scripts\activate
+```
--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost:3306/suivis"
+# Updated connection string
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:EB199944@mysql.com@localhost:3306/appReact"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
